c650ef57ecbbf97161b2130bf39968f7dfa92329
Giljm <Giljm@Giljm.com>
2021-02-02 14:29:11 +0900

    수업내용

---
+++ java_001/src/com/callor/hello/HelloTime.java
@@ -0,0 +1,30 @@
+package com.callor.hello;
+
+public class HelloTime {
+
+	public static void main(String[] args) {
+		
+		//Sysout 컨트롤 스페이스를 누르면 System.out.println();을 자동으로 불러옴
+		System.out.println("3 x 1 = " + 3 * 1);
+		System.out.println("3 x 2 = " + 3 * 2);
+		System.out.println("3 x 3 = " + 3 * 3);
+		System.out.println("3 x 4 = " + 3 * 4);
+		System.out.println("3 x 5 = " + 3 * 5);
+		System.out.println("3 x 6 = " + 3 * 6);
+		System.out.println("3 x 7 = " + 3 * 7);
+		System.out.println("3 x 8 = " + 3 * 8);
+		System.out.println("3 x 9 = " + 3 * 9);
+
+		System.out.println("6 x 1 = " + 6 * 1);
+		System.out.println("6 x 2 = " + 6 * 2);
+		System.out.println("6 x 3 = " + 6 * 3);
+		System.out.println("6 x 4 = " + 6 * 4);
+		System.out.println("6 x 5 = " + 6 * 5);
+		System.out.println("6 x 6 = " + 6 * 6);
+		System.out.println("6 x 7 = " + 6 * 7);
+		System.out.println("6 x 8 = " + 6 * 8);
+		System.out.println("6 x 9 = " + 6 * 9);
+		
+	}
+
+}
---
+++ java_002/src/java_002/java_002.java
@@ -0,0 +1,13 @@
+package java_002;
+
+public class java_002 {
+	
+	public static void main(String[] args) {
+			System.out.println("30 + 40 = 70");
+			System.out.println("30 * 40 = 1200");
+			System.out.println("40 - 30 = 10");
+			System.out.println("40 / 20 = 2");
+	
+	}
+
+}
