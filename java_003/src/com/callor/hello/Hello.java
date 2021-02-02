2d2b8049a5a0cce7dbe28275106a8668b0094517
Giljm <Giljm@Giljm.com>
2021-02-02 09:44:29 +0900

    java_003 연습

---
+++ java_003/src/com/callor/hello/Hello.java
@@ -0,0 +1,26 @@
+package com.callor.hello;
+
+public class Hello {
+
+	/*
+	 * 메인(main) 메소드(method)라고 부른다
+	 * 런(Run)을 했을때 꼭 필요한 코드
+	 * 이클립스에서 런을 했을때 발생하는 상황
+	 * Hello.java 파일을 컴파일 : javac Hello.java
+	 * Hello.class 파일이 생성되면
+	 * java.Hello 명령을 실행하여 코드를 컴퓨터에 주입하고 결과를 콘솔에 출력
+	 */
+	
+		public static void main (String args[])
+		{ 
+			System.out.println("30 + 40 = " + 70);
+			System.out.println("30 + 40 = " + 1200);
+			System.out.println("40 - 30 = " + (40 - 30));
+			System.out.println("40 / 20 = " + 2);
+			System.out.println();
+			System.out.println("여러분 반갑습니다");
+			
+		}
+	}
+
+	
