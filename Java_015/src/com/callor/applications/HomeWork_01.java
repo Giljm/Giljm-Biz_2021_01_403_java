be9021d08bb04e11ebd7f6b9985fd226d220c89d
Giljm <qnfrhetbt2@naver.com>
2021-02-24 09:05:23 +0900

    Prime

--- Java_015/src/com/callor/applications/HomeWork_01.java
+++ Java_015/src/com/callor/applications/HomeWork_01.java
@@ -16,28 +16,34 @@ public class Homework_01 {
 		int index = 0;
 		for (index = 0; index < Num.length; index++) {
 			Num[index] = rnd.nextInt(150) + 1;
-		}
+		} // 랜덤 2 ~ 150의 값을 intNum 0 ~ 99에 담음.
+	      // 임의의 정수 100개 생성.
+
 
 		int[] result = new int[100];
 		for (index = 0; index < Num.length; index++) {
 			result[index] = psV2.prime(Num[index]);
-		}
+		} // psV2의 prime() method의 결과를 result 0 ~ 99에 담음.
+	      // 임의의 소수와 소수가 아닌 -1을 생성.
+
 
 		for (index = 0; index < Num.length; index++) {
 
 			if (result[index] < 0) {
-
+				// prime() method에서 나온 -1이라는 결과의 출력을 걸러줌.
 			} else {
 				System.out.printf("%d은(는) 소수다\n", result[index]);
-			}
+			}  // 소수 들만 걸러서 출력.
+
 		}
 
 		int sum = 0;
 		for (index = 0; index < Num.length; index++) {
 			if (result[index] < 0) {
-
+				// prime() method에서 나온 -1이라는 결과의 출력을 걸러줌.
 			} else {
 				sum += result[index];
+				// prime() method에서 나온 소수들만 누적해서 더함.
 			}
 		}
 
